9af50a3eb2ab5a44c3783554dfb708b9b352e382
nimbs <nimbs@microsoft.com>
2017-09-21 22:47:22 +0300

    1st version for winsetup.ps1

--- setup.ps1
+++ setup.ps1
@@ -9,14 +9,15 @@
 # Temp
 Disable-UAC
 
+# Win disable
 Disable-BingSearch
 Disable-GameBarTips
 
 # Install tools
-choco update sysinternals -y
+choco install sysinternals -y
 
 # Install apps
-choco update googlechrome
+choco install googlechrome
 choco install notepadplusplus.install
 choco install python2
 choco install visualstudiocode
@@ -25,6 +26,48 @@ choco install visualstudiocode
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+# Uninstall software
+$packages = @(  "Microsoft.BingFinance", 
+                "Microsoft.BingNews", 
+                "Microsoft.BingSports", 
+                "Microsoft.BingWeather",
+                "Microsoft.ZuneVideo",
+                "Microsoft.XboxApp",
+                "Microsoft.ZuneVideo",
+                "Microsoft.WindowsAlarms",
+                "king.com.CandyCrush*",
+                "*Facebook*",
+                "*Minecraft*",
+                "*Solitaire*",
+                "*Twitter*")
+$packages | ForEach-Object { Get-AppxPackage $_ | Remove-AppxPackage }
+
+# Start Menu: Disable Bing Search Results
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
+# To Restore (Enabled):
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 1
+
+
+# Change Explorer home screen back to "This PC"
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 1
+# Change it back to "Quick Access" (Windows 10 default)
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Type DWord -Value 2
+
+# Better File Explorer
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1		
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1		
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+# These make "Quick Access" behave much closer to the old "Favorites"
+# Disable Quick Access: Recent Files
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowRecent -Type DWord -Value 0
+# Disable Quick Access: Frequent Folders
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowFrequent -Type DWord -Value 0
+# To Restore:
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowRecent -Type DWord -Value 1
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowFrequent -Type DWord -Value 1
 
 
 # Restore Temp
